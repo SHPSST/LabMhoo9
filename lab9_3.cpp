3a9ce60071c8de7c6520981310d2d62ea7f4f64e
Hutao Doctrine <guy_zxc@hotmail.com>
2021-12-07 22:05:44 +0700

    Modified

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,11 +1,52 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your property: "
-"Your status = "
-"UNFRIEND"
-"FRIEND"
-"BEST FRIEND"
-"ONE-NIGHT-STAND"
-"MARRIED"
-*/
+#include <iostream>
+using namespace std;
+int main()
+{ 
+    int age,height,prop;
+    cout << "Enter your age: ";
+    cin >> age;
+    if(age<=20)
+        {
+            cout << "Enter your height: ";
+            cin >> height;
+            if(height<160)
+                {
+                    cout << "Your status = UNFRIEND";
+                }
+           else if(height<175 && height>159)
+                {
+                    cout << "Your status = FRIEND";
+                }
+            else
+                {
+                    cout << "Enter your property: ";
+                    cin >> prop;
+                    if(prop>69000000)
+                        {
+                            cout << "Your status = MARRIED";
+                        }
+                    else
+                        {
+                            cout << "Your status = ONE-NIGHT-STAND";
+                        }
+                }
+        }
+    else if(age<=30)
+        {
+            cout << "Enter your property: ";
+            cin >> prop;
+            if(prop>10000000)
+                        {
+                            cout << "Your status = BEST FRIEND";
+                        }
+                    else
+                        {
+                            cout << "Your status = UNFRIEND";
+                        }
+
+        }
+    else
+        {
+            cout << "Your status = UNFRIEND";
+        }
+}
